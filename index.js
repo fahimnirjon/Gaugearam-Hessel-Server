0200eec093e3e9612d958479a68bc53723a1337a
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-26 22:49:44 +0600

    added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken')
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -49,6 +50,25 @@ async function run() {
       }
       const result = await userCollection.insertOne(user);
       res.send(result) 
+    })
+    // admin
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
     })
 
     // menu api
